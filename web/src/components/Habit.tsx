98c98d472a618e4e997ad9afb7f2ce0d5f3442dd
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-01-16 23:24:15 -0300

    created development environment for server and web

---
+++ web/src/components/Habit.tsx
@@ -0,0 +1,13 @@
+import "../input.css";
+
+interface HabitProps {
+  completed: number;
+}
+
+export function Habit(props: HabitProps) {
+  return (
+    <div className="bg-zinc-900 w-10 h-10 text-white m-2 flex items-center rounded justify-center">
+      {props.completed}
+    </div>
+  );
+}
