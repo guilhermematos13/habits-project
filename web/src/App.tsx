6f1802fefd9e2841b078d6e5256002f72f34e234
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-01-18 19:34:02 -0300

    feat: create header component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,7 @@
-import { Habit } from "./components/Habit";
+// import { Habit } from "./components/Habit";
 
-function App() {
-  return (
-    <div>
-      <Habit completed={3} />
-      <Habit completed={10} />
-      <Habit completed={20} />
-      <Habit completed={30} />
-    </div>
-  );
+import { Header } from "./components/Header";
+
+export function App() {
+  return <Header />;
 }
-
-export default App;
