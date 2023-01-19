9520e273bf1bedd71683eff8c04ed41d7b02da60
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-01-18 23:22:24 -0300

    feat: create home screen and other components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,6 +8,7 @@ import {
 } from "@expo-google-fonts/inter";
 import { Loading } from "./src/components/Loading";
 import { Home } from "./src/screens/Home";
+import { HabitDay } from "./src/components/HabitDay";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
